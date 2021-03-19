66efa37e4e27745efc702a31711e9c5d6b670b45
Luke Curran <luke.peter.curran54@gmail.com>
2021-03-19 10:58:03 -0500

    updated external.js

--- js/external.js
+++ js/external.js
@@ -1,44 +1,48 @@
+"use strict";
+
+// Part 1
+console.log("Hello from external Javascript!")
+
 alert('Welcome to my Website!');
 
+// Part 2
 var favoriteColor = prompt("What is your favorite color?");
 
-alert("Great " + favoriteColor + " is my favorite color too!");
+alert("Great, " + favoriteColor + " is my favorite color too!");
 
-// Master CopyPasta
-// var favoriteCheese = prompt("What is your favorite cheese?");
+// Part 3
 
 // Bullet 1
-var lMRentalInDays = prompt("How many days did you rent Little Mermaid?");
-var bBRentalInDays = prompt("How many days did you rent Brother Bear?");
-var hercRentalInDays = prompt("How many days did you rent Hercules?");
-var rentalRate = prompt("How much are you charged per day for renting a movie?");
+var lMRentalInDays = Math.ceil(parseFloat(prompt("How many days did you rent Little Mermaid?")));
+var bBRentalInDays =  Math.ceil(parseFloat(prompt("How many days did you rent Brother Bear?")));
+var hercRentalInDays =  Math.ceil(parseFloat(prompt("How many days did you rent Hercules?")));
+
+var rentalRate =  Math.ceil(parseFloat(prompt("How much are you charged per day for renting a movie?")));
+
 var totalDue = (lMRentalInDays * rentalRate) + (bBRentalInDays * rentalRate) + (hercRentalInDays * rentalRate)
-alert(totalDue + " is your total for renting these movies");
+
+alert("$ " + (totalDue.toFixed (2)) + " is your total for renting these movies");
 
 //Bullet 2
-var googleRate = prompt("How much does Google pay hourly?");
-var amazonRate = prompt("How much does Amazon pay hourly?");
-var fBRate = prompt("How much does Facebook pay hourly?");
-var googleHours = prompt("How many hours did you work at Google?");
-var amazonHours = prompt("How many hours did you work at Amazon?");
-var fBHours = prompt("How many hours did you work at Facebook?");
-var totalPay = (googleHours * googleRate) + (amazonHours * amazonRate) + (fBHours * fBRate);
-alert(totalPay + " is your total pay from these companies");
+var googleRate = parseFloat(prompt("How much does Google pay hourly?"));
+var googleHours = parseFloat(prompt("How many hours did you work at Google?"));
+
+var amazonRate = parseFloat(prompt("How much does Amazon pay hourly?"));
+var amazonHours = parseFloat(prompt("How many hours did you work at Amazon?"));
+
+var fBRate = parseFloat(prompt("How much does Facebook pay hourly?"));
+var fBHours = parseFloat(prompt("How many hours did you work at Facebook?"));
+
+alert("You made $ " + ((googleHours * googleRate) + (amazonHours * amazonRate) + (fBHours * fBRate)));
 
 // Bullet 3
-var classSize = prompt("How many students can sign up for a class?");
-var studentsSignedUp = prompt("How many students have signed up for this class?");
-var classTime = prompt("What class time works for you?");
-var classStartTimes = prompt("What class times are available?");
-var haveClassAt = false;
-var classFull = false;
-var canRegister = !(haveClassAt || classFull);
-false || true
-alert(canRegister + "  you can sign up for this class.");
+var classHasRoom = confirm("Is there room for this class?");
+var noScheduleConflict = confirm("Does this class slot work for you?");
+
+alert("You can register: " + (classHasRoom && noScheduleConflict));
 
 // Bullet 4
-var offerValid = prompt("Is the offer valid for you?");
-var boughtAtLeastTwo = prompt("Have you bought at least two items?");
-var premiumMember = prompt("Are you a premium member?");
-var canHasDiscount = (premiumMember || boughtAtLeastTwo) && (offerValid);
-alert(canHasDiscount + " you are eligible for the discount");
+var isOfferValid = prompt("Is the offer valid for you?");
+var amountBought = parseFloat(prompt("Have you bought at least two items?"));
+var isPremiumMember = prompt("Are you a premium member?");
+alert("Customer qualifies for promo: " + (((isPremiumMember || amountBought >= 2) && isOfferValid) && amountBought >= 1) + ".")

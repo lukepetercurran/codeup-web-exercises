ff1e701f91fc43714d4ef5d0a892c1870fbb640a
Luke Curran <luke.peter.curran54@gmail.com>
2021-05-17 13:58:40 -0500

    after lunch exercise push

--- js/map-filter-reduce-lec.js
+++ js/map-filter-reduce-lec.js
@@ -49,7 +49,74 @@ let prices = [32.99, 21.99, 6.99, 4.99, 12.99, 8.98, 5.99];
 //    $("#output").html(output);
 //    return thing;
 // });
+
+// ES5
+// pluralThings.forEach(function(thing, index){
+//     if (index === 0) {
+//         output = "<p>You must bring the following items:</p>";
+//         output = output + "<p>" + thing + "</p>";
+//     }
+//     output = output + "<p>" + thing + "</p>";
+//     $("#output").html(output);
+// });
+// ES6
+// let things = ["book", "pencil", "marker", "eraser"];
+// let pluralThings = things.map((thing, index) => {
+//     if (index === 0) {
+//         output = "<p>You must bring the following items:</p>";
+//     }
+//     output += `<p>${thing}s</p>`;
+//     $("#output").html(output);
+// });
+
+// const cars = [
+//     {
+//         make: "Honda",
+//         model: "Civic",
+//         mileage: 10428
+//     },
+//     {
+//         make: "Toyota",
+//         model: "Corolla",
+//         mileage: 9425
+//     },
+//     {
+//         make: "Ford",
+//         model: "Mustang",
+//         mileage: 2567
+//     },
+//     {
+//         make: "Audi",
+//         model: "A3",
+//         mileage: 14500
+//     },
+//     {
+//         make: "Mazda",
+//         model: "3",
+//         mileage: 11248
+//     }
+// ];
+
+// ES5
+// var mileages = cars.map(function(car){
+//         return car.mileage;
+// });
+// ES6
+// const mileages = cars.map((car) => car.mileage);
 //
+// mileages.forEach(function(mileage){
+//     output = output + "<p>" + mileage + "</p>";
+//     $("#output").html(output);
+// });
+// .filter()
+// const under10K = cars.filter(function(car){
+//     return car.mileage < 10000;
+// });
+//
+// under10K.forEach(function(car){
+//     output = output + "<p>I found a " + car.make + " " + car.model + " with " + car.mileage + " miles.</p>";
+//     $("#output").html(output);
+
 var affordablesWithTax = prices.filter(function(price){
    return price < 10;
 }).map(function(price){
@@ -57,10 +124,9 @@ var affordablesWithTax = prices.filter(function(price){
    let total = (parseFloat(price) + parseFloat(tax)).toFixed(2);
    return parseFloat(total);
 });
-
-affordablesWithTax.forEach(function(itemPrice){
+affordablesWithTax.forEach(function(itemPrice) {
    output = output + "<p>" + itemPrice + "</p>";
-   $("#output").html(output)
+   $("#output").html(output);
 });
 
 // ===== Reduce =====
@@ -93,3 +159,106 @@ totalCost = prices.reduce(function(total, price, index){
 
 console.log(totalCost.toFixed(2));
 
+
+   const aesopAnimals = ["Ant", "Grasshopper", "Ape", "Fox", "Donkey", "Donkey", "Donkey", "Lion", "Fly", "Bear", "Beaver", "Blackbird", "Wolf", "Bull", "Lion", "Cat", "Mouse", "Crab", "Fox", "Rooster", "Rooster", "Dog", "Fox", "Crow", "Crow", "Sheep", "Crow", "Snake", "Deer", "Dog", "Dog", "Sheep", "Dog", "Wolf", "Dog", "Lion", "Dove", "Ant", "Eagle", "Fox", "Eagle", "Stork", "Viper", "Fish", "Fly", "Ant", "Fly", "Snake", "Fox", "Crow", "Fox", "Fox", "Lion", "Fox", "Fox", "Lion", "Fox", "Stork", "Fox", "Weasel", "Fox", "Fly", "Hedgehog", "Hare", "Frog", "Fox", "Frog", "Mouse", "Frog", "Ox", "Frog", "Frog", "Goat", "Goose", "Hare", "Horse", "Donkey", "Horse", "Kite", "Dove", "Lion", "Lion", "Lion", "Lion", "Bear", "Fox", "Lion", "Boar", "Vulture", "Dog", "Mouse", "Oyster", "Donkey", "Ox", "Kite", "Snake", "Crab", "Snake", "Snake", "Tortoise", "Tortoise", "Hare", "Cat", "Wolf", "Wolf", "Lamb", "Swallow", "Tortoise"];
+
+   let aesopsAnimalsCounted = aesopAnimals.reduce(function(total, animal){
+      // total begins as an empty object
+      // the animal parameter represents each item in the array, as it is passed to the reduce function iterating through that array. So on its first pass, animal = "Ant", then "Grasshopper", etc.
+      // Since "total" is an empty object, on the first pass, this code sets a key called Ant, and its value is 1
+      //{ Ant: 1 }
+      // Grasshopper: 1
+      // Ape: 1
+      if(!total[animal]) {
+         total[animal] = 1;
+      } else {
+         total[animal] = total[animal] + 1;
+      }
+      return total;
+   }, {});
+
+   console.log(aesopsAnimalsCounted);
+
+   const aesopsAnimalsCountedAndSorted = Object.entries(aesopsAnimalsCounted).sort(function(a, b){return b[1]-a[1]});
+
+   for (let i = 0; i < aesopsAnimalsCountedAndSorted.length; i++){
+      $("#output").append(
+          aesopsAnimalsCountedAndSorted[i][0] + ": " + aesopsAnimalsCountedAndSorted[i][1] + "<br>"
+      );
+   }
+
+   const cars = [
+      {
+         make: "Honda",
+         model: "Civic",
+         mileage: 10428
+      },
+      {
+         make: "Toyota",
+         model: "Corolla",
+         mileage: 9425
+      },
+      {
+         make: "Ford",
+         model: "Mustang",
+         mileage: 2567
+      },
+      {
+         make: "Audi",
+         model: "A3",
+         mileage: 14500
+      },
+      {
+         make: "Mazda",
+         model: "3",
+         mileage: 11248
+      }
+   ];
+
+   const mileages = cars.reduce(function(accumulator, car){
+      accumulator.push(car.mileage);
+      return accumulator;
+   }, []);
+
+   console.log(mileages);
+
+   const highestMileage = cars.reduce(function(accumulator, car, index, carsArray){
+      if (index === carsArray.length - 1) {
+         accumulator.push(car.mileage);
+         accumulator.sort(function(a, b){return b-a});
+         return accumulator[0];
+      } else {
+         accumulator.push(car.mileage);
+         return accumulator;
+      }
+   }, []);
+
+   console.log("The highest mileage is " + highestMileage);
+
+   /*We could do this differently, with a reduce within a reduce. Since our accumulator is an array, on our last iteration we can run a reduce function on it. This is a standard reduce function that takes, by default, the first value in the array as the initial accumulator variable. It then takes each mileage stored in that accumulator array. The function we run here simply uses Math.max to compare the two values passed to reduce on each iteration, and return the highest one. This will compare 10428 to 9425 and return 10428, then compare 10428 to 2567 and return 10428, then compare 10428 to 14500 and return 14500, finally compare 14500 to 11248 and return 14500. Then we return the value stored as a variable resulting from that reduce within the reduce, and we also arrive at the highest mileage in the cars array.*/
+
+
+// get lowest and highest values in a numerical array
+   prices = [32.99, 21.99, 6.99, 4.99, 12.99, 8.98, 5.99];
+
+   let highLowPrices = prices.reduce(function(accumulatorArray, price, index, pricesArray){
+      if (index === pricesArray.length-1){
+         accumulatorArray.push(price);
+         let returnArray = [];
+         accumulatorArray.sort(function(price1, price2){
+            return price1 - price2;
+         });
+         let lowestPrice = accumulatorArray[0];
+         let highestPrice = accumulatorArray[accumulatorArray.length-1];
+         returnArray.push(lowestPrice);
+         returnArray.push(highestPrice);
+         return returnArray;
+      } else {
+         accumulatorArray.push(price);
+         return accumulatorArray;
+      }
+   }, []);
+
+   console.log(highLowPrices);
+
+// array.slice(0,4).reduce();
--- js/map-filter-reduce.js
+++ js/map-filter-reduce.js
@@ -36,7 +36,7 @@ const users = [
     }
 ];
 
-// 2
+// #2
 
 let es5multiLingual = users.filter(function(user){
         return users.languages.length >= 3;
@@ -59,6 +59,73 @@ let es5Emails = users.map(function(user){
 let es6Emails = users.map(user => user.email);
 
 // #4
-let totalEXP = users.reduce(function(total,yearsOfExperience, users){
-    return user
-})
+
+//es5
+
+let totalEXP = users.reduce(function(total, user){
+    let yearsOfExperience = user.yearsOfExperience
+    return total + yearsOfExperience
+}, 0);
+
+console.log(totalEXP);
+
+//es6
+
+totalEXP = users.reduce((total, user)=> total + user.yearsOfExperience, 0);
+console.log(totalEXP)
+
+let emailsViaReduce = users.reduce(function(accumulator, user){
+    accumulator.push(user.email);
+    return accumulator
+}, []);
+
+let longestEmail = users.reduce(function(accumulator, user, index, usersArray){
+    if (index === usersArray.length -1){
+        accumulator.push(user.email);
+        accumulator.sort(function(email1, email){
+            return email2.length - email.length;
+        });
+        return accumulator[0]
+    } else {
+        accumulator.push(user.email);
+        return accumulator
+    }
+}, []);
+
+console.log(longestEmail);
+
+// const longestEmail = users.reduce((longest, current) =>{
+//     if(current.email.length > longest.email.length){
+//         return current;
+//     } else {
+//         return longest;
+//     }
+// })  //David Culvers Method
+
+// let longestUserEmail = users.reduce((a, b) => {
+//     return a.length > b.email.length ? a : b.email;
+// }); // Matthews Method
+
+// #5
+
+let names = users.reduce(function(accumulator, user){
+    accumulator.push(user.name);
+    return accumulator;
+}, []);
+
+let namesNicelyJoined = users.reduce(function(accumulator, user,index, usersArray){
+    if (index === uderrsArray.length - 1){
+        accumulator.push(user.name);
+        let niceJoin = accumulator.join(', ');
+    } else {
+        accumulator.push(user.name);
+        return accumulator
+    }
+}, []);
+
+console.log(namesNicelyJoined);
+
+let longestUserEmailByKarenV = users.reduce((a, b) => {
+    return a.length > b.email.length ? a : b.email;
+});
+console.log(longestUserEmailByKarenV)

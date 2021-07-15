b8fbe5409918d47fca40dad6344e9d4067d35575
Luke Curran <luke.peter.curran54@gmail.com>
2021-07-15 11:41:12 -0500

    technical interview

---
+++ js/Technical Interview.js
@@ -0,0 +1,15 @@
+// log out fizz if x % 3 buzz %5, fizzbuzz if both, just number if neither
+
+
+for (i < 16, i = 0, i++){
+    if (x/3 === 0 && x/5 === 0)
+        console.log("fizzbuzz")
+    if (x/3 === 0)
+        console.log("fizz")
+    if (x/5 === 0)
+        console.log("buzz")
+    else
+        console.log(0)
+}
+
+// joshua peters

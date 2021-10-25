11934d5b1a519a0dcd71c168e883728414954c68
Luke Curran <luke.peter.curran54@gmail.com>
2021-10-25 11:59:58 -0500

    had to reup intellij subscription

--- js/warmups.js
+++ js/warmups.js
@@ -24,27 +24,40 @@
 
 // Warmup May 26, Write a function in Javascript that takes in a number and returns the number passed multiplied by the number 5. If we pass anything other than a number into the function, the function should return the number 0.
 
-function fiveByFive(x){
-    if (x <= 0 || x > 0) {
-        return (x * 5)
-    } else {
-        return (0)
-    }
-}
-
-const multiplyByFive = num => typeof num === "number" ? num * 5 :0;
-
-console.log(multiplyByFive(5));
-console.log(multiplyByFive(-3.5));
-console.log(multiplyByFive(0));
-console.log(multiplyByFive("Sixteen"));
-console.log(multiplyByFive(true));
+// function fiveByFive(x){
+//     if (x <= 0 || x > 0) {
+//         return (x * 5)
+//     } else {
+//         return (0)
+//     }
+// }
+//
+// const multiplyByFive = num => typeof num === "number" ? num * 5 :0;
+//
+// console.log(multiplyByFive(5));
+// console.log(multiplyByFive(-3.5));
+// console.log(multiplyByFive(0));
+// console.log(multiplyByFive("Sixteen"));
+// console.log(multiplyByFive(true));
 
 
 // I miss these warmups
 // I still miss these warmups
 
-
+const fizzbuzz = numTil => {
+    for (let x = 1; x<= numTil; x++){
+        if(x % 3 === 0 && x % 5 === 0){
+            console.log("Fizzbuzz");
+        } else if (x % 3 === 0){
+            console.log("Fizz")
+        } else if (x % 5 === 0){
+            console.log("Buzz")
+        } else {
+            console.log(x);
+        }
+    }
+}
+fizzbuzz(100)
 
 
 
